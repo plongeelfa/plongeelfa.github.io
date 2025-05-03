1e48dcee5a17233c5d203e982f6f5ec5fc0830a6
plongeelfa <morini.elisa.1@gmail.com>
2025-05-03 15:38:35 +0200

    Add files via upload

---
+++ codes/Untitled-2.php
@@ -0,0 +1,39 @@
+<!DOCTYPE htlm>
+<htlm lan="fr">
+    <head>
+        <metacharset="utf-8">
+        <title>Seconde page</title>
+    </head>
+    <body>
+        <h1>Et l'environnement alors ?</h1>
+        <h2>Quelques regles de base</h2>
+        <p>blah blah bla</p>
+        <p>blah blah bla</p>
+        <p>blah blah bla</p>
+        <p>blah blah bla</p>
+        <p>blah blah bla</p>
+        <p>blah blah bla</p>
+        <p>blah blah bla</p>
+        <p>blah blah bla</p>
+        <p>blah blah bla</p>
+        <p>blah blah bla</p>
+        <h2>Un respect obligatoire</h2>
+        <p>blah blah bla</p>
+        <p>blah blah bla</p>
+        <p>blah blah bla</p>
+        <p>blah blah bla</p>
+        <p>blah blah bla</p>
+        <p>blah blah bla</p>
+        <p>blah blah bla</p>
+        <p>blah blah bla</p>
+        
+        <h2>Pensons à l'avenir</h2>
+        <p>blah blah bla</p>
+        <p>blah blah bla</p>
+        <p>blah blah bla</p>
+        <p>blah blah bla</p>
+        <p>blah blah bla</p>
+        <p>blah blah bla</p>
+        <p>blah blah bla</p>
+    </body>
+<>htlm>
